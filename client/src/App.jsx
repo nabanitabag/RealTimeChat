aa888d003a5fb20aa1f4bdf6776c5b7374921dfc
Nabanita Bag <ashley.makruz@gmail.com>
2021-10-06 18:08:24 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Alert } from "shards-react";
+ 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+import "./index.css";
+
+const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
